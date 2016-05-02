baccad570a7855166b6685a427fd43610b80bf8e
Mathew Winters <mathew@winters.org.nz>
2016-05-02 18:52:58 +1200

    Updates for travis

--- wct.conf.js
+++ wct.conf.js
@@ -6,33 +6,5 @@ module.exports = {
       browsers: ['chrome']
       //, 'firefox']
     },
-    sauce: {
-      disabled: true,
-      browsers: [
-        'Linux/chrome',
-        'Linux/firefox',
-        'OS X 10.10/chrome',
-        'OS X 10.10/firefox',
-        'OS X 10.10/safari',
-        {
-            platform: 'Windows 7',
-            browserName: 'internet explorer',
-            version: '10.0',
-        },
-        {
-            platform: 'Windows 7',
-            browserName: 'internet explorer',
-            version: '11.0',
-        },
-        {
-            platform: 'Windows 10',
-            browserName: 'MicrosoftEdge',
-        },
-
-        'Windows 7/chrome',
-        'Windows 10/firefox',
-
-      ]
-    }
   },
 };
