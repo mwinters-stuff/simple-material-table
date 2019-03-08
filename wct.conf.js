ca1c47a01b05c758e8650b5d2a9aaad98d33c922
Mathew Winters <mathew_winters@trimble.com>
2019-03-08 15:41:41 +1300

    travis test e.t.c.

--- wct.conf.js
+++ wct.conf.js
@@ -1,10 +1,15 @@
 module.exports = {
 //  verbose: true,
   testTimeout: 5 * 60 * 1000,
+  npm: true,
+
   plugins: {
     local: {
+      browserOptions: {
+        chrome: ["--headless", "--disable-gpu", "--no-sandbox"],
+        firefox: ["--headless"]
+      },
       browsers: ['chrome']
-      //, 'firefox']
     },
   },
 };
